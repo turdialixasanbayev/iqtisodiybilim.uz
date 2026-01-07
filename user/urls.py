708420d50f3f058b02979b2da431b40e7a34f569
Turdiali <turdialixasanbayev@gmail.com>
2026-01-07 21:36:44 +0500

    url path updated

--- user/urls.py
+++ user/urls.py
@@ -21,17 +21,17 @@ from .views import (
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('dashboard/', ProfileView.as_view(), name='dashboard'),
     path('verify/', VerifyView.as_view(), name='verify'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('login/', LoginView.as_view(), name='login'),
     path('verify/resend/', ResendOTPView.as_view(), name='resend-otp'),
     path('delete-account/', UserDeleteView.as_view(), name='delete-account'),
     path('profile-update/', ProfileUpdateView.as_view(), name='profile-update'),
     path("password-update/", PasswordUpdateView.as_view(), name="password-update"),
     path('email-update/', UpdateEmailView.as_view(), name='email-update'),
-    path('email-update-verify/', VerifyEmailUpdateView.as_view(), name='verify-email-update'),
-    path('email-update-resend/', ResendEmailUpdateOTPView.as_view(), name='resend-email-update-otp'),
+    path('email-update/verify/', VerifyEmailUpdateView.as_view(), name='verify-email-update'),
+    path('email-update/resend/', ResendEmailUpdateOTPView.as_view(), name='resend-email-update-otp'),
 
     path(
         'forgot-password/',
@@ -40,19 +40,19 @@ urlpatterns = [
     ),
 
     path(
-        'forgot-password-verify/',
+        'forgot-password/verify/',
         VerifyForgotPasswordView.as_view(),
         name='verify-forgot-password'
     ),
 
     path(
-        'forgot-password-resend/',
+        'forgot-password/resend/',
         ResendForgotPasswordOTPView.as_view(),
         name='resend-forgot-password-otp'
     ),
 
     path(
-        'forgot-password-reset/',
+        'forgot-password/reset/',
         ResetPasswordView.as_view(),
         name='reset-password'
     ),
