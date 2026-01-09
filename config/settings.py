679aeaccae6eb205057b752eadde6bc1e49dce15
Turdiali <turdialixasanbayev@gmail.com>
2026-01-09 18:16:48 +0500

    Updated

--- config/settings.py
+++ config/settings.py
@@ -9,8 +9,7 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 
 SECRET_KEY = env("SECRET_KEY")
 
-# DEBUG = env.bool('DEBUG')
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
